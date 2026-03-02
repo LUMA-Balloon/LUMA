fc4361ad90aa5c5f1d120a5d26906b87db33f895
Ian Kelley <240542958+iankelleyfreelance@users.noreply.github.com>
2026-03-01 20:55:10 -0600

    Updated LRC test code for groundstation

--- src/lrc/lrc_test.cpp
+++ src/lrc/lrc_test.cpp
@@ -1,2 +1,18 @@
-// NOT YET
+#include <Arduino.h>
 
+void setup() {
+    Serial.begin(115200);
+    Serial.println("ECHO 1");
+    Serial.println("MYCALL K4UAH");
+    Serial.println("MYALIAS LRC_TEST");
+    Serial.println("MON all");
+}
+
+void loop() {
+    test_lrc();
+    delay(1000);
+}
+
+void test_lrc() {
+    Serial.println("UNPROTO DROP_TEST");
+}
