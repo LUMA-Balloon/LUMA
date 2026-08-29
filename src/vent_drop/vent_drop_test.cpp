28531f2b1140c5ac04ace400a77dcc8be1e3112d
Sa9m <abyssalflerken@gmail.com>
2026-08-29 15:31:13 -0500

    Should just be closed

--- src/vent_drop/vent_drop_test.cpp
+++ src/vent_drop/vent_drop_test.cpp
@@ -146,6 +146,11 @@ void loop() {
 
   digitalWrite(dropEnable, HIGH);
   digitalWrite(ventEnable, HIGH);
+
+
+  myServo.write(180); // Always closed
+
+
   // digitalWrite(LED_BUILTIN, HIGH);
   // for (int i = 0; i < 1023; i++) {
   //   digitalWrite(dropServoPin, i);
@@ -159,17 +164,19 @@ void loop() {
   //   delay(1);
   // }
   // digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
 
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myServo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  digitalWrite(LED_BUILTIN, HIGH);
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myServo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  digitalWrite(LED_BUILTIN, LOW);
+  // delay(1000);
+
+  // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  //   // in steps of 1 degree
+  //   myServo.write(pos);              // tell servo to go to position in variable 'pos'
+  //   delay(15);                       // waits 15ms for the servo to reach the position
+  // }
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  //   myServo.write(pos);              // tell servo to go to position in variable 'pos'
+  //   delay(15);                       // waits 15ms for the servo to reach the position
+  // }
+  // digitalWrite(LED_BUILTIN, LOW);
+
 }
